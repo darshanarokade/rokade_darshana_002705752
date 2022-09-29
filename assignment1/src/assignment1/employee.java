2446ef8fdae858c452b61f5cf063840601cc2f78
Darshana Rokade <rokadedarshana47@gmail.com>
2022-09-28 20:26:21 -0400

    Adding INFO Assignment One

---
+++ assignment1/src/assignment1/employee.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment1;
+
+/**
+ *
+ * @author HP
+ */
+
+public class employee {
+    
+    private String name;
+    private long empid;
+    private int age;
+    private String gender;
+    //private date startdate;
+    private String level;
+    private String teaminfo;
+    private String positiontitle;
+    private String contactinfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public long getEmpid() {
+        return empid;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    /*public date getStartdate() {
+        return startdate;
+    
+    }*/
+
+    public String getLevel() {
+        return level;
+    }
+
+    public String getTeaminfo() {
+        return teaminfo;
+    }
+
+    public String getPositiontitle() {
+        return positiontitle;
+    }
+
+    public String getContactinfo() {
+        return contactinfo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmpid(long empid) {
+        this.empid = empid;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /*public void setStartdate(date startdate) {
+        this.startdate = startdate;
+    }*/
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public void setTeaminfo(String teaminfo) {
+        this.teaminfo = teaminfo;
+    }
+
+    public void setPositiontitle(String positiontitle) {
+        this.positiontitle = positiontitle;
+    }
+
+    public void setContactinfo(String contactinfo) {
+        this.contactinfo = contactinfo;
+    }
+    
+         
+}
